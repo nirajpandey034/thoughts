d72f56e63efb85706dedeaefeb98b437d0ab8c4f
Rahul Tiwari <rahul.tiwari@Triconinfotech.com>
2021-06-03 11:46:47 +0530

    Initial file structure

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => {
     res.send('API is running');
-})
+});
+
+// Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
